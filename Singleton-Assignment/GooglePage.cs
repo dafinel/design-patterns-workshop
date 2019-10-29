fec3d24c588517f792e9c6407cecb1d2a07b51bc
Anton <andrei.anton@centric.eu>
2019-10-29 13:29:52 +0200

    singleton-assignment

---
+++ Singleton-Assignment/GooglePage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Singleton
+{
+    public class GooglePage
+    {
+        private readonly IWebDriver _webDriver;
+
+        public GooglePage(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        private IWebElement _seachOnInput;
+
+        public void Load()
+        {
+            _webDriver.Navigate().GoToUrl("https://www.google.ro/?gws_rd=ssl");
+            var wait = new WebDriverWait(_webDriver,
+                System.TimeSpan.FromSeconds(15));
+            wait.Until(driver =>
+                _seachOnInput = driver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")));
+        }
+
+        public void SearchOn(string text)
+        {
+            _seachOnInput.SendKeys(text);
+            _seachOnInput.SendKeys(Keys.Enter);
+        }
+    }
+}
---
+++ Singleton-Assignment/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Singleton;
+
+namespace Singleton_Assignment
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver _driver;
+
+        [TestMethod]
+        public void SearchOnGoogle()
+        {
+            var googlePage = new GooglePage(_driver);
+            googlePage.Load();
+            googlePage.SearchOn("centric it solutions iasi");
+
+            var searchResultPage = new GoogleSeachResultPage(_driver);
+            searchResultPage.Load();
+
+            Assert.AreEqual(searchResultPage.FirstSearchTitle, "Centric IT Solutions Romania - Acasă | Facebook");
+        }
+
+
+        [TestCleanup]
+        public void TearDownTest()
+        {
+            _driver.Close();
+        }
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            _driver = new ChromeDriver();
+        }
+    }
+}
